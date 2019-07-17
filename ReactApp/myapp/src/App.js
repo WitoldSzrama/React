6d98e723476a9d7fc65e15b3c7a7176313b00247
Witek <witold.szrama@gmail.com>
2019-07-17 15:40:40 +0200

    playing with forms

---
+++ ReactApp/myapp/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component }from 'react';
+import Ninjas from './ninjas';
+import AddNinja from './AddNinja'
+
+class App extends Component {
+  state = {
+    ninjas : [	
+      {name: 'Ryu', age:30 , belt:'black', id:1},
+      {name: 'Yoshi', age:20 , belt:'green', id:2},
+      {name: 'Crystal', age:25 , belt:'pink', id:3}
+    ]	
+  }
+  addNinja = (ninja) =>{
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas:ninjas
+    })
+  }
+  deleteNinja = (id) =>{
+    let ninjas = this.state.ninjas.filter(ninja =>ninja.id !== id)
+    this.setState({
+      ninjas:ninjas
+    })
+  }
+  componentDidMount(){
+    console.log('component mounted');
+  }
+  componentDidUpdate(prevProps, prevState){
+    console.log(" updated");
+    console.log(prevProps,prevState); 
+  }
+  render() {
+  return (
+    <div className="App">
+        <h1>My first React App!</h1>
+        <p>Welcome</p>
+        <Ninjas deleteNinja={this.deleteNinja}ninjas={this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja}/>
+    </div>
+    );
+  }
+}
+export default App;
